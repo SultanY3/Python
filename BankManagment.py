838e89b1c5c5ccc56ba3103bb3303235d80fc803
Sultan <iamsultanyousuf@gmail.com>
2025-06-04 10:47:53 +0530

    Second

---
+++ BankManagment.py
@@ -0,0 +1,70 @@
+accounts = []
+a = ''
+while a != '5':
+    print("1. Create Account")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Show Account")
+    print("5. Exit")
+
+    a = input("Select an action: ")
+    if a == '1':
+        name = input("Store Name: ")
+        number = input("Account Number: ")
+        acc = 0
+        for i in accounts:
+            if i['number'] == number:
+                acc = 'yes'
+
+        if acc == 'yes':
+            print("Account number already exists")
+        else:
+            balance = float(input("Initial Balance: "))
+            accounts.append({'name': name, 'number': number, 'balance': balance})
+            print("Account created")
+
+    elif a == '2':
+        number = input("Account Number: ")
+        for i in accounts:
+            if i['number'] == number:
+
+                amount = float(input("Deposit Amount: "))
+                if amount > 0:
+                    i['balance'] += amount
+                    print("Amount Deposited")
+                else:
+                    print("Enter positive number!")
+            else:
+                print("Account not available!")
+
+    elif a == '3':
+        number = input("Account Number: ")
+        for i in accounts:
+            if i['number'] == number:
+                amount = float(input("Withdraw Amount: "))
+                if amount > 0:
+                    if amount <= i['balance']:
+                        i['balance'] -= amount
+                        print("Amount Withdrawn")
+                    else:
+                        print("Not enough balance")
+                else:
+                    print("Enter positive number")
+            else:
+                print("Account not available")
+
+    elif a == '4':
+        number = input("Account Number: ")
+        for i in accounts:
+            if i['number'] == number:
+                print("Name:", i['name'])
+                print("Account Number:", i['number'])
+                print("Balance:", i['balance'])
+            else:
+                print("Account not available")
+
+    elif a == '5':
+        print("Exiting program...")
+
+    else:
+        print("Invalid input, please try again.")
---
+++ shape.py
@@ -0,0 +1,98 @@
+# n=5
+# for i in range(0,n):
+#     for j in range(0,n):
+#         print("* ",end=" ")
+#     print()  
+
+# n=5
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print("* ",end="")
+#     print()  
+
+# n=5
+# for i in range(0,n):
+#     for j in range(i,n):
+#         print("* ",end=" ")
+#     print()   
+
+# n=5
+# for i in range(0,n):
+#     for j in range(0,n-i):
+#         print(" ",end="")
+#     for k in range(0,i+1):
+#         print("*",end="")
+#     print() 
+
+# n=5
+# for i in range(0,n):
+#     for j in range(0,n-i):
+#         print(" ",end="")
+#     for k in range(0,i+1):
+#         print("* ",end="")
+#     print() 
+
+# n=5
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print(" ",end="")
+#     for k in range(0,n-i):
+#         print("* ",end="")
+#     print() 
+
+# n=5
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print(" ",end="")
+#     for k in range(0,n-i):
+#         print("*",end="")
+#     print()
+
+# n=5
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print(" ",end="")
+#     for k in range(0,n):
+#         print("*",end="")
+#     print()    
+
+# n=5
+# a=1
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print(a,end=" ")
+#         a+=1
+#     print()   
+
+# n=6
+# a=4
+# for i in range(0,a):
+#     for j in range(0,n-i):
+#         print(" ",end="")
+#     for k in range(0,i+1):
+#         print("* ",end="")
+#     print()    
+# for l in range(0,a-1):
+#     for m in range(0,a+l):
+#         print(" ",end="")
+#     for p in range(1,a-l):
+#         print("* ",end="") 
+#     print()     
+
+# n=5
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print(" ",end="")
+#     for k in range(0,n-i):
+#         print("* ",end="")
+#     print() 
+# for i in range(0,n-1):
+#     for j in range(0,n-i-1):
+#         print(" ",end="")
+#     for k in range(0,i+2):
+#         print("* ",end="")
+#     print() 
+
+    
+  
+
