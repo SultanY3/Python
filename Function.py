3c081dc1e1018fe0c5c71b0051c0cab6ec2cfd25
Sultan <iamsultanyousuf@gmail.com>
2025-06-11 12:35:38 +0530

    ninth

---
+++ Function.py
@@ -0,0 +1,125 @@
+# def square(num):
+#     return num**2
+# object_=square(6)
+# print("The square of the given number is:",object_)
+
+# def square(num):
+#     return num**2
+# object_=square(int(input("Enter a number:")))
+# print("The square of the given number is:",object_)
+
+# def square(num):
+#     return num**2
+# a=int(input("Enter a number:"))
+# b=square(a)
+# print(b)
+
+# def a_function(string):
+#     "This prints the value of length of string"
+#     return len(string)
+# #calling the function we defined
+# print(a_function("Functions"))
+# print(a_function("Python"))
+###########################
+# #with arg with ret type
+
+# #fn to calculate square of a number
+# def square(num):
+#     return num * num
+
+# result=square(5)
+# print("Square:",result)
+##########################
+# #with arg without ret type
+
+# #fn to print greeting msg
+# def greet(name):
+#     print("Hello",name+"!")
+# greet("Anu")
+##########################
+# #without arg with ret type 
+
+# #fn that returns a welcome msg
+# def get_msg():
+#     return "Welcome to python programming" 
+ 
+# msg=get_msg()
+# print(msg)
+##########################
+# #without arg without ret type
+
+# #fn to print numbers from 1 to 5 
+# def print_num():
+#     for i in range(1,6):
+#         print(i)
+# print_num()
+##########################
+#1
+# def square(num):
+#     return num * num
+# result=square(int(input("Enter a number:")))
+# print("Square:",result)
+#2
+# def sum_(num,num1):
+#     return num + num1
+# a=int(input("Enter 1st number:"))
+# b=int(input("Enter 2nd number:"))
+# result=sum_(a,b)
+# print("Sum of both numbers:",result)
+#3
+# def upper_fn(string):
+#     return string.upper()
+# a=input("Enter the string:")
+# b=upper_fn(a)
+# print(b)
+#4
+# def greet(name):
+#     print("Hello",name+"!")
+# a=input("Enter name:")
+# greet(a)
+#5
+# def mult(n1,n2):
+#     print("porduct:",n1*n2)
+# n1=int(input("Enter 1st num:"))
+# n2=int(input("Enter 2nd num:"))
+# mult(n1,n2)
+#6
+# def even(num):
+#     if num%2==0:
+#         print("Even")
+#     else:
+#         print("Odd")
+# n=int(input("Enter num:"))
+# even(n)
+#7
+# def get_year():
+#     return 2025
+# a=get_year()
+# print("Current year:",a)
+#8
+# def get_msg():
+#     return "Welcome to python programming" 
+# msg=get_msg()
+# print(msg)
+#9
+# def fact_5():
+#     result = 1
+#     for i in range(1,6):
+#         result = result * i
+#     return result
+# print("Factorial:",fact_5())  
+#10
+# def nam():
+#     print("your name is:",i)
+# i=input("enter your name:")
+# nam()
+#11
+# def print_num():
+#     for i in range(1,11):
+#         print(i)
+# print_num()
+
+
+
+
+
--- question.py
+++ question.py
@@ -1,13 +1,13 @@
-unit=int(input("Enter the amount of units used: "))
-price=0
-if unit>0 and unit<=100:
-    price=unit*5 
-    print("Bill:",price)
-elif unit>100 and unit<=200:
-    price=(100*5)+((unit-100)*8) 
-    print("Bill:",price)   
-elif unit>200:
-    price=(100*5)+(100*8)+((unit-200)*10) 
-    print("Bill:",price)  
-else:
-    print("Enter a valid amount!")
+# unit=int(input("Enter the amount of units used: "))
+# price=0
+# if unit>0 and unit<=100:
+#     price=unit*5 
+#     print("Bill:",price)
+# elif unit>100 and unit<=200:
+#     price=(100*5)+((unit-100)*8) 
+#     print("Bill:",price)   
+# elif unit>200:
+#     price=(100*5)+(100*8)+((unit-200)*10) 
+#     print("Bill:",price)  
+# else:
+#     print("Enter a valid amount!")
