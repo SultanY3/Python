cfa04bf6c413467a910fd7bf511d5dfe9ddb90bc
Sultan <iamsultanyousuf@gmail.com>
2025-06-17 11:49:31 +0530

    fourteenth

---
+++ filehandling.py
@@ -0,0 +1,44 @@
+# f=open('Newfile.txt','x')   #create new file
+
+# file=open('Newfile.txt','r')  #open file and print all contents using for loop
+# for each in file:
+#     print(each)
+
+# file=open('Newfile.txt','r')  #open file and print contents line by line using file.readline()
+# print(file.readline())
+# print(file.readline())
+# file.close()
+
+# file=open('Newfile.txt','r')  #open file and print all contents
+# print(file.read())
+
+# with open('Newfile.txt') as file:  #open file and print all contents using with fn
+#     data=file.read()
+# print(data)
+
+# file=open('Newfile.txt','r')  #open file and print certain no of characters
+# print(file.read(5))
+
+# with open('Newfile.txt') as file:  #open file and print all contents using with fn we can split lines using split() fn
+#     data=file.readlines()
+#     for line in data:
+#         word=line.split()
+#         print(word)
+
+# file=open('Newfile.txt','w')  #overrides the file contents and inputs new text
+# file.write('This is the write command')
+# file.write('It allows us to write in a particular file')
+# file.close()
+
+# file=open('Newfile.txt','a')  #appends the file contents and inputs new text (existing content will not be removed)
+# file.write('Check check one two')
+# file.write('testing in progress')
+# file.close()
+
+
+
+
+
+
+
+
