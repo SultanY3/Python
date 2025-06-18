753ea6dbc8fdfd2e11fb542b3af2345a8e4464e5
Sultan <iamsultanyousuf@gmail.com>
2025-06-18 12:05:47 +0530

    15th

--- new2.py
+++ new2.py
@@ -8,3 +8,30 @@
 
 # import math as mt
 # print("The value of eulers number is:",mt.e)
+
+# import math
+# x=6
+# print("Factorial:",math.factorial(x))
+
+# import time
+# print(time.asctime(time.localtime(time.time())))
+
+# import time
+# for i in range(0,5):
+#     print(i)
+# time.sleep(1)
+
+# import datetime
+# print(datetime.datetime.now())
+
+# import datetime
+# print(datetime.datetime(2020,12,4))
+
+# import calendar
+# cal=calendar.month(2004,2)
+# print(cal)
+
+# import calendar
+# cal=calendar.calendar(2020)
+# print(cal)
+
