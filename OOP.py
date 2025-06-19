f525829b1131b71e2871323d26508621c37afba9
Sultan <iamsultanyousuf@gmail.com>
2025-06-19 11:12:02 +0530

    Sixteenth

---
+++ OOP.py
@@ -0,0 +1,21 @@
+#creating objects:
+
+# class Employee:
+#     id=10
+#     name="John"
+#     def display(self):
+#         print("ID:%d\nName:%s"%(self.id,self.name))
+# emp=Employee()
+# emp.display()
+
+#deleting objects:
+
+# class Employee:
+#     id=10
+#     name="John"
+#     def display(self):
+#         print("ID:%d\nName:%s"%(self.id,self.name))
+# emp=Employee()
+# del emp.id
+# del emp.name
+# emp.display()
