9b7d8ad6ecbdecda22a127f3aa337a560fe03a5e
Sultan <iamsultanyousuf@gmail.com>
2025-06-21 20:04:09 +0530

    Eighteenth

--- inheritence.py
+++ inheritence.py
@@ -160,3 +160,22 @@
 # obj2.flight()
 # obj3.intro()
 # obj3.flight()
+
+# ><><><><><><><><><><><>< Abstract ><><><><><><><><><><><><
+
+# from abc import ABC, abstractmethod
+# class Animal(ABC):   # Abstract class
+#     @abstractmethod
+#     def make_sound(self):
+#         pass # Abstract method
+# class Dog(Animal): # Concrete class
+#     def make_sound(self):
+#         return "Woof!"
+# class Cat(Animal): # Concrete class
+#     def make_sound(self):
+#         return "Meow!"
+# # Client code
+# dog=Dog()
+# cat=Cat()
+# print(dog.make_sound()) # Output: Woof!
+# print(cat.make_sound()) # Output: Meow!
